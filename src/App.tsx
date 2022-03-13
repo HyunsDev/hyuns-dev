807aa4c79fd3cd8be77d196490184ca2b3cc9f70
혀느현스 <hyuns@hyuns.dev>
2022-03-13 20:50:14 +0900

    rotation rockct

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, Suspense, useState, useEffect } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
+import styled from 'styled-components'
+import Model from './assets/Rocket'
+
+import * as THREE from 'three'
+
+const Div = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  position: relative;
+  height: 500px;
+  width: 500px;
+`
+
+function getRadian(degree: number) {
+  console.log(degree)
+  return degree * Math.PI / 180
+}
+
+function Rig({ children }:any) {
+  const outer = useRef<any>()
+  const inner = useRef<any>()
+  useFrame(({ clock }) => {
+    inner.current.rotation.y = Math.sin(clock.getElapsedTime() / 8) * Math.PI * 8
+  })
+  return (
+    <group position={[0, -100, 0]} ref={outer}>
+      <group ref={inner}>{children}</group>
+    </group>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Canvas gl={{ preserveDrawingBuffer: true }} shadows dpr={[1, 1.5]} camera={{ position: [0, 0, 150], fov: 50 }}>
+        <Suspense fallback={null}>
+          <Stage
+            contactShadow={false}
+            intensity={0.3}
+          >
+            <Rig>
+              <Model />
+            </Rig>
+            
+          </Stage>
+        </Suspense>
+      </Canvas>
+    </Div>
   );
 }
 
 export default App;
+
