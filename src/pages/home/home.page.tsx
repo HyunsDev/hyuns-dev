84b4356edd622fdcf7a752c9ea856cb5a113def2
혀느현스 <hyuns@hyuns.dev>
2024-03-26 02:32:37 +0900

    feat: 디테일 추가

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -8,23 +8,33 @@ import { AboutStackSection } from "./sections/Stack.section";
 import { AboutICanDoItSection } from "./sections/ICanDoIt.section";
 import { AboutContactSection } from "./sections/Contact.section";
 
+import { motion } from "framer-motion";
+
 export function HomePage() {
   return (
     <>
       <MainHeader />
-      <div className="">
-        <HeroSection />
-        <AboutMottoSection />
-        <AboutDescriptionSection />
-        <AboutStackSection />
-        <AboutICanDoItSection />
-        <AboutContactSection />
-      </div>
-      <LoadingOverlay
-        texts={["새로운 상상을 하고", "상상을 현실로 만듭니다"]}
-        time={2500}
-      />
-      <BackgroundCanvas />
+      <motion.div
+        className="page"
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+        }}
+      >
+        <div className="">
+          <HeroSection />
+          <AboutMottoSection />
+          <AboutDescriptionSection />
+          <AboutStackSection />
+          <AboutICanDoItSection />
+          <AboutContactSection />
+        </div>
+        <LoadingOverlay
+          texts={["새로운 상상을 하고", "상상을 현실로 만듭니다"]}
+          time={2500}
+        />
+        <BackgroundCanvas />
+      </motion.div>
     </>
   );
 }
